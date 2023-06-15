def5747440c4eba39ba10946a43822b1a1c63645
Johann Severin <johann.bock.severin@hotmail.com>
2023-06-15 09:18:29 +0200

    moved tests and playground to master thesis repository

--- playground/module_test.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-sys.path.append("..")
-
-from devices.device import Resonator, Transmon
-from devices.pulses import SquareCosinePulse
-from devices.system import QubitResonatorSystem
-
-from simulation.experiment import SchroedingerExperiment, LindbladExperiment
